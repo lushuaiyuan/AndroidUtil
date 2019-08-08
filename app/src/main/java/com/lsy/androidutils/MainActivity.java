8a4a8493ed8ae12160c01c7f165bba13bbc4cfec
lushuaiyuan <lsy_itsports@163.com>
2019-08-08 18:29:19 +0800

    AndroidUtil——QMUITopBarLayout的集成

--- app/src/main/java/com/lsy/androidutils/MainActivity.java
+++ app/src/main/java/com/lsy/androidutils/MainActivity.java
@@ -1,10 +1,14 @@
 package com.lsy.androidutils;
 
-import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.lsy.androidutils.base.BaseActivity;
@@ -19,18 +23,17 @@ import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindString;
 import butterknife.BindView;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     @BindView(R.id.topbar)
     QMUITopBarLayout topBar;
     @BindView(R.id.navigationBar)
     EasyNavigationBar navigationBar;
 
 
-    private String[] tabText = {"首页", "发现", "消息", "我的"};
+    private String[] tabText = {"首页", "发现", "消息", "我"};
     //未选中icon
     private int[] normalIcon = {R.mipmap.home_normal, R.mipmap.find_normal, R.mipmap.message_normal, R.mipmap.mine_normal};
     //选中时icon
@@ -45,20 +48,11 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void init() {
-        topBar.setTitle(tabText[0]);
+        initTopBar();
+        initBottomBar();
+    }
 
-        topBar.addLeftImageButton(R.mipmap.icon_back, 0).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "返回", Toast.LENGTH_SHORT).show();
-            }
-        });
-        topBar.addRightImageButton(R.mipmap.icon_share, 0).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "分享", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void initBottomBar() {
         fragments.add(new HomeFragment());
         fragments.add(new FindFragment());
         fragments.add(new MessageFragment());
@@ -66,12 +60,74 @@ public class MainActivity extends BaseActivity {
         navigationBar.titleItems(tabText)
                 .normalIconItems(normalIcon)
                 .selectIconItems(selectIcon)
+                .lineHeight(1)
+                .lineColor(ContextCompat.getColor(this, R.color.bar_divider))
+                .onTabClickListener(new EasyNavigationBar.OnTabClickListener() {
+                    @Override
+                    public boolean onTabClickEvent(View view, int position) {
+                        topBar.setTitle(tabText[position]);
+                        return false;
+                    }
+                })
                 .fragmentList(fragments)
                 .fragmentManager(getSupportFragmentManager())
                 .canScroll(true)
-                .navigationHeight(48)
+//                .navigationHeight(50)
+                .anim(Anim.ZoomIn)
+
                 .build();
+        // ((WeiboActivity) getActivity()).getNavigationBar().selectTab(1); //指定跳转位置
+        //TODO 设置小红点和消息
+        navigationBar.setMsgPointCount(0, 109);
+        navigationBar.setMsgPointCount(3, 5);
+
+        navigationBar.setHintPoint(1, true);
+
+        //移除消息和小红点
+//        navigationBar.clearHintPoint(position);//移除指定位置的小红点
+//        navigationBar.clearAllHintPoint();//移除所有的小红点
+//        navigationBar.clearMsgPoint(position);//移除指定位置的消息
+//        navigationBar.clearAllMsgPoint();//移除所有的消息
     }
 
+    private void initTopBar() {
+        topBar.setBackgroundColor(ContextCompat.getColor(this, R.color.app_color_theme_2));
+        topBar.setTitleGravity(Gravity.LEFT);
+        topBar.setTitle("收件箱");
+        topBar.setSubTitle("lsy_itsports@163.com");
+        topBar.addLeftImageButton(R.mipmap.icon_list, 0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "侧边栏", Toast.LENGTH_SHORT).show();
+            }
+        });
+        View view = LayoutInflater.from(this).inflate(R.layout.view_home_right, null);
+        ImageButton ibFilter = view.findViewById(R.id.ib_filter);
+        ibFilter.setOnClickListener(this);
+        ImageButton ibSearch = view.findViewById(R.id.ib_search);
+        ibSearch.setOnClickListener(this);
+        ImageButton ibAdd = view.findViewById(R.id.ib_add);
+        ibAdd.setOnClickListener(this);
+        topBar.addRightView(view,R.id.home_right);
+    }
+
+    public EasyNavigationBar getNavigationBar() {
+        return navigationBar;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_add:
+                Toast.makeText(MainActivity.this, "添加", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_filter:
+                Toast.makeText(MainActivity.this, "过滤", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_search:
+                Toast.makeText(MainActivity.this, "查询", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
 
